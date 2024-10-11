2d2177adbb428f9658a71c7528cfe1861fd72b60
juliane <s215843@student.dtu.dk>
2024-10-11 14:39:43 +0200

    donor

--- src/main/java/data/TestCreate.java
+++ src/main/java/data/TestCreate.java
@@ -1,5 +1,7 @@
 package data;
 
+import data.Entities.Donor;
+import data.Entities.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -9,21 +11,32 @@ import org.testng.annotations.Test;
 public class TestCreate {
 
     public void testCreate() {
+        //opretter forbindelse til hybernate controlleren
         HibernateController hibernateController =
                 new HibernateController("pgdatabase.donor.4a4b.dk:5432/postgres");
         SessionFactory sessionFactory = hibernateController.getSessionFactory();
         Session session = sessionFactory.openSession();
+        //starter en transaktion
         Transaction transaction = session.beginTransaction();
+
+        //opretter en ny bruger
         User user = new User();
+        Donor donor = new Donor();
         System.out.println("UserID before commit: " + user.getId());
         user.setUsername("henrik");
+        donor.setFirstname("Harry");
+        //gemmer brugeren i databasaen
         session.persist(user);
+        session.persist(donor);
         transaction.commit();
+        //printer det nye ID
         System.out.println("UserID after commit: " + user.getId());
+        //læser brugeren tilbage fra database
         Transaction readTransaction = session.beginTransaction();
         User readUser = session.get(User.class, user.getId());
         System.out.println("Read user back: " + readUser.toString());
         readTransaction.commit();
+        //lukker sessionen
         session.close();
     }
 }
---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,40 @@
+package service;
+
+import data.Entities.User;
+import data.HibernateController;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class UserService {
+
+    private HibernateController hibernateController;
+
+    // Dependency Injection via constructor
+    public UserService(HibernateController hibernateController) {
+        this.hibernateController = hibernateController;
+    }
+
+    public List<User> getAllUsers() {
+        // Henter session fra Hibernate
+        Session session = hibernateController.getSessionFactory().openSession();
+        Transaction tx = null;
+        List<User> users = null;
+
+        try {
+            tx = session.beginTransaction();
+            // HQL-forespørgsel for at hente alle brugere
+            users = session.createQuery("from DBUSER", User.class).getResultList();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        return users;
+    }
+
+}
