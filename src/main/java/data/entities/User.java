814e92d471a86086c41fa093469d79ab454f952d
juliane <s215843@student.dtu.dk>
2024-12-16 09:14:21 +0100

    user entitet opdateret

--- src/main/java/data/entities/User.java
+++ src/main/java/data/entities/User.java
@@ -11,6 +11,14 @@ public class User {
     @Column(name = "username")
     private String username;
 
+    @Column (name= "password")
+    private String password;
+
+    // Constructor
+    public User() {}
+    public User( String username, String password) { }
+
+
     // TODO: Remember Getters and setters as well
     // Getter for id
     public int getId() {
@@ -28,4 +36,9 @@ public class User {
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public String getPassword() {return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
 }
